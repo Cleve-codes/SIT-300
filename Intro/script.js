f6db4aa0767dd7799178c1e2c44c456a2875e9f8
Cleve <clevemomanyi@gmail.com>
2023-10-03 08:35:44 +0300

    intro

---
+++ Intro/script.js
@@ -0,0 +1,68 @@
+// // ///////////////////////////////////////////////
+// // // Task 1
+// // const largestEl = document;
+// // const BASE_LENGTH = 400;
+// // const HEIGHT = 100;
+
+// // // function findArea() {
+// // //   const area = (1 / 2) * BASE_LENGTH * HEIGHT;
+// // //   areaEl.textContent = area;
+// // //   console.log(`The area of the rectangle is ${area}`);
+// // // }
+// // // findArea();
+
+// // //////////////////////////////
+// // // Task 2
+// // function compareNumbers() {
+// //   const num1 = parseFloat(prompt("Enter the first number: "));
+// //   const num2 = parseFloat(prompt("Enter the second number: "));
+// //   const num3 = parseFloat(prompt("Enter the third number: "));
+
+// //   let largest = 0;
+
+// //   if (num1 >= num2 && num1 >= num3) {
+// //     //largest is num 1;
+// //     largest = num1;
+// //   } else if (num2 >= num1 && num2 >= num3) {
+// //     //the largest is num2
+// //     largest = num2;
+// //   } else {
+// //     //the largest is num3
+// //     largest = num3;
+// //   }
+
+// //   if (num1 === num2 && num2 === num3) {
+// //     console.log("the numbers are equal");
+// //   }
+
+// //   console.log(`The largest number is ${largest}`);
+// //   //console.log(typeof num1);
+// //   largestEl.value = largest;
+// //   return largest;
+// // }
+
+// // compareNumbers();
+
+// function hello() {
+//   alert("Hello world");
+// }
+
+// function validation() {
+//   alert("validation");
+// }
+
+// /////////////////////////////////////////
+// // Object
+// var book = new Object();
+// book.subject = "C";
+// book.author = "mohtashim";
+// console.log("book name is:" + book.subject + "<br>");
+// console.log("book author is:" + book.author + "<br>");
+
+////////////////////////////////////////////
+// Task
+// Programme to capture students Marks using an array.
+function marksArray(){
+  const marks = [];
+  const userMarks = parseFloat(prompt("Enter your marks below..."))
+}
